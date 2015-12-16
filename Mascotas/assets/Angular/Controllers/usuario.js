c2a2d0da419f42ae283a1059bcdd6b6f2c0ce2b9
Adrian Eguez <eadepto@hotmail.com>
2015-12-15 20:56:37 -0500

    Mejorar Interfaz y Anadir funcion agregar Entrenador y Actualizar Entrenador

--- Mascotas/assets/Angular/Controllers/usuario.js
+++ Mascotas/assets/Angular/Controllers/usuario.js
@@ -3,6 +3,48 @@
 
 applicacion.controller('usuariocontroller',['$scope','$http',function($scope,$http){
     
+//    $scope.nombrePersonal = "Pepito";
+    
+    $scope. nuevoUsuario = {
+        nombre:'',
+        apellido:'',
+        fechaNacimiento:''
+    }
+    
+    $scope.agregarNuevoEntrenador = function(){
+        
+        $http({
+            method:'POST',
+            url:'http://localhost:1337/Entrenador',
+            data:{
+                nombre: $scope.nuevoUsuario.nombre,
+                apellido: $scope.nuevoUsuario.apellido,
+                fechaNacimiento: $scope.nuevoUsuario.fechaNacimiento
+            }
+        }).then(
+        function correctoLlamoEntrenadores(respuesta){
+            
+            console.log(respuesta);
+            
+            $scope.entrenadores.push(respuesta.data);
+            
+            $scope. nuevoUsuario = {
+                                        nombre:'',
+                                        apellido:'',
+                                        fechaNacimiento:''
+                                    }
+            
+        },
+        function errorNoLlamoEntrenadores(error){
+            console.log(error);
+        });
+        
+        
+        
+        
+    }
+    
+    
     console.log('Entraste a usuario');
     
     $http({
@@ -21,7 +63,42 @@ applicacion.controller('usuariocontroller',['$scope','$http',function($scope,$ht
         });
     
     
-    
+    $scope.editarEntrenador = function (entrenador, indice){
+        console.log(entrenador);
+        console.log(indice);
+        var fecha;
+
+        if(entrenador.nuevaFecha==null)
+            {
+                fecha = $scope.entrenadores[indice].fechaNacimiento;    
+            }
+        else
+        {
+            
+            fecha = entrenador.nuevaFecha;
+            
+        }
+        
+        $http({
+            method:'PUT',
+            url:'http://localhost:1337/Entrenador/'+entrenador.id,
+            data:{
+                nombre: entrenador.nombre,
+                apellido: entrenador.apellido,
+                fechaNacimiento: fecha
+            }
+        }).then(
+        function correctoLlamoEntrenadores(respuesta){
+            
+             console.log(respuesta);
+            $scope.entrenadores[indice].fechaNacimiento=fecha;
+            
+            
+        },
+        function errorNoLlamoEntrenadores(error){
+            console.log(error);
+        });
+    };
     
     
     
@@ -30,3 +107,15 @@ applicacion.controller('usuariocontroller',['$scope','$http',function($scope,$ht
     
     
 }]);
+
+
+
+
+
+
+
+
+
+
+
+
